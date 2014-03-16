61e914a160e43cb94acccc32e8e34711d930662a
Allan Ritchie <allan.ritchie@gmail.com>
2014-03-15 23:04:38 -0400

    More nuspec stuff

--- nuspec/pack.bat
+++ nuspec/pack.bat
@@ -5,7 +5,7 @@ nuget pack Acr.Ninject.nuspec
 nuget pack Acr.Autofac.nuspec
 nuget pack Acr.NetFx.nuspec
 nuget pack Acr.Ef.nuspec
-nuget pack Acr.Nh.nuspecs
+nuget pack Acr.Nh.nuspec
 nuget pack Acr.Mail.nuspec
 nuget pack Acr.Mail.NVelocityParser.nuspec
 nuget pack Acr.Mail.RazorParser.nuspec
