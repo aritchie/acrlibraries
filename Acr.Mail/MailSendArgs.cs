3aa729e35212f15e0a1ca85a8892945b835b2f17
Allan Ritchie <allan.ritchie@gmail.com>
2014-07-13 13:38:01 -0400

    Missed file

---
+++ Acr.Mail/MailSendArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Acr.Mail {
+    
+    public class MailSendArgs {
+
+        public string TemplateName { get; set; }
+        public CultureInfo Culture { get; set; }
+        public object Model { get; set; }
+    }
+}
